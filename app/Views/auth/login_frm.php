75f326ddac8df1d849627804fcbc297a47895a4d
Vicente <pvicenteneto7@gmail.com>
2024-04-23 16:43:11 -0300

    feat: base do projeto

---
+++ app/Views/auth/login_frm.php
@@ -0,0 +1,4 @@
+<?= $this->extends('layouts/layout_auth') ?>
+<?= $this->section('content') ?>
+
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/layout_auth.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('assets/libs/bootstrap/bootstrap.min.css') ?>">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/libs/fontawesome/all.min.css') ?>">
+
+    <!-- css -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/login.css') ?>">
+
+</head>
+<body>
+    <!-- render section -->
+    <?= $this->renderSection('content') ?>
+
+
+    <!-- bootstrap js -->
+    <script src="<?= base_url('assets/libs/bootstrap/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
